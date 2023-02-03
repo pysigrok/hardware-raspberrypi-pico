28031dc3f63088531fc7bfb9824eb82710e452b0
Scott Shawcroft <scott@tannewt.org>
2023-02-03 15:28:54 -0800

    Capture works including 5+ pins. (not analog)

--- pysigrok_raspberrypi_pico.py
+++ pysigrok_raspberrypi_pico.py
@@ -4,7 +4,7 @@ __version__ = "0.0.1"
 
 import serial
 
-from sigrokdecode import SR_KHZ, SR_MHZ
+from sigrokdecode import SR_KHZ, SR_MHZ, cond_matches
 
 class PicoDriver:
     name = "raspberrypi-pico"
@@ -63,20 +63,17 @@ class PicoDriver:
         SR_MHZ(240)
     ]
 
-    def __init__(self, channellist="", conn=""):
+    def __init__(self, channellist=None, conn="", samplerate="5000"):
         self.serial = serial.Serial(conn)
-        self._samplerate = 5000
+        self._samplerate = int(samplerate)
         self.data = []
 
         self.serial.reset_input_buffer()
         self.serial.write(b"*")
         self.serial.reset_input_buffer()
 
-        print("requesting id")
         self.serial.write(b"i\n")
         info = self.serial.read(17)
-        print(info)
-        print(len(info))
         if not info.startswith(b"SRPICO,A"):
             raise RuntimeError()
         _, channelinfo, self.version = info.split(b",")
@@ -85,12 +82,23 @@ class PicoDriver:
         self.num_a_channels = int(channelinfo[1:3])
         self.analog_bytes = int(channelinfo[3:4])
         self.num_d_channels = int(channelinfo[-2:])
-        print(self.num_a_channels, self.analog_bytes, self.num_d_channels)
+        # print(self.num_a_channels, self.analog_bytes, self.num_d_channels)
 
-        self.analog_channels = [f"A{n:d}" for n in range(self.num_a_channels)]
-        self.logic_channels = [f"D{n+2:d}" for n in range(self.num_d_channels)]
+        self.all_analog_channels = [f"A{n:d}" for n in range(self.num_a_channels)]
+        self.all_logic_channels = [f"D{n+2:d}" for n in range(self.num_d_channels)]
 
-        self.enabled_channels = ["D16", "D17"]
+        if channellist is None:
+            self.enabled_channels = self.all_logic_channels + self.all_analog_channels
+        else:
+            self.enabled_channels = channellist.split(",")
+
+        self.last_sample = None
+        self.next_sample = None
+        self.samplenum = 0
+        self.rle_remaining = 0
+        self.data_index = 0
+        self.chunk_index = 0
+        self.overall_index = 0
 
     def __del__(self):
         self.serial.close()
@@ -114,15 +122,21 @@ class PicoDriver:
             raise RuntimeError()
 
     def acquire(self, sample_count):
+        self.serial.reset_input_buffer()
+        self.serial.write(b"*")
+        self.serial.reset_input_buffer()
 
         digital_enables = ["D"] * self.num_d_channels
-        for channel in self.analog_channels:
-            i = self.analog_channels.index(channel)
+        for channel in self.all_analog_channels:
+            i = self.all_analog_channels.index(channel)
             enabled = 1 if channel in self.enabled_channels else 0
             self.send_w_ack(f"A{enabled:d}{i:d}\n")
-        for channel in self.logic_channels:
-            i = self.logic_channels.index(channel)
+        first_enabled = None
+        for channel in self.all_logic_channels:
+            i = self.all_logic_channels.index(channel)
             enabled = 1 if channel in self.enabled_channels else 0
+            if enabled == 1 and first_enabled is None:
+                first_enabled = i
             self.send_w_ack(f"D{enabled:d}{i:d}\n")
             if enabled:
                 digital_enables[i] = "E"
@@ -130,6 +144,9 @@ class PicoDriver:
         digital_enables = digital_enables.strip("D")
         if "D" in digital_enables:
             raise RuntimeError("Digital pins must be continuous")
+
+        self.logic_channel_count = len(digital_enables)
+        self.logic_channels = self.all_logic_channels[first_enabled:first_enabled+len(digital_enables)]
 
         self.send_w_ack(f"L{sample_count:d}\n")
 
@@ -145,10 +162,126 @@ class PicoDriver:
         self.serial.write(b"F\n")
 
         samples_read = 0
-        while samples_read < sample_count:
+        last_value = 0
+        byte_count = 0
+        trailer = None
+        while trailer is None:
             while self.serial.in_waiting == 0:
                 pass
             data = self.serial.read(self.serial.in_waiting)
-            print(data)
-            samples_read += len(data)
+            if b"!" in data:
+                self.serial.write(b"+")
+                continue
+            if b"$" in data:
+                trailer = data
+                break
+            if trailer is None:
+                self.data.append(data)
+        if not trailer.startswith(b"$"):
+            raise RuntimeError()
+        while b"+" not in trailer:
+            trailer += self.serial.read(self.serial.in_waiting)
+        total_bytes = int(trailer[1:trailer.index(b"+")])
+        if sum((len(x) for x in self.data)) != total_bytes:
+            raise RuntimeError()
 
+    def _next_byte(self):
+        if self.data_index >= len(self.data):
+            self.samplenum += self.rle_remaining + 1
+            raise EOFError()
+        chunk = self.data[self.data_index]
+        b = chunk[self.chunk_index]
+        self.chunk_index += 1
+        self.overall_index += 1
+        if self.chunk_index >= len(chunk):
+            self.chunk_index = 0
+            self.data_index += 1
+        return b
+
+
+    def wait(self, conds=None):
+        self.matched = [False]
+
+        next_sample = None
+
+        while not any(self.matched):
+            # Check for a skip shorter than our RLE or a condition that still matches
+            if self.rle_remaining > 0:
+                self.matched = [False] * len(conds)
+                for i, cond in enumerate(conds):
+                    if cond_matches(cond, self.last_sample, self.last_sample):
+                        self.matched[i] = True
+                        sample = self.last_sample
+                
+                if any(self.matched):
+                    self.rle_remaining -= 1
+                else:
+                    for i, cond in enumerate(conds):
+                        if "skip" in cond and cond["skip"] <= self.rle_remaining:
+                            self.matched[i] = True
+                            self.rle_remaining -= cond["skip"]
+                            self.samplenum += cond["skip"]
+                            sample = self.last_sample
+                            # Only one skip is possible
+                            break
+
+            if any(self.matched):
+                break
+
+            self.samplenum += self.rle_remaining
+            self.rle_remaining = 0
+
+            if self.next_sample is not None:
+                sample = self.next_sample
+                self.next_sample = None
+            else:
+                b = self._next_byte()
+                if self.logic_channel_count <= 4:
+                    # RLE byte
+                    if b & 0x80 != 0:
+                        sample = b & 0x0f
+                        previous_length = (b >> 4) & 0x7
+                        if previous_length > 0:
+                            # We have more rle remaining and might match a skip.
+                            self.rle_remaining = previous_length
+                            self.next_sample = sample
+                            sample = self.last_sample
+                            continue
+                    elif 0x30 <= b < 0x80:
+                        previous_length = (b - 0x30 + 1) * 8
+                        self.rle_remaining += previous_length
+                        continue
+                else:
+                    # Start of a new sample
+                    if b & 0x80 != 0:
+                        sample = b & 0x7f
+                        for i in range(self.logic_channel_count // 7):
+                            b = self._next_byte()
+                            if b & 0x80 == 0:
+                                raise RuntimeError(f"Expected more sample {b:02x}")
+                            sample |= (b & 0x7f) << (7 * (i + 1))
+                    else:
+                        # RLE byte
+                        if 48 <= b <= 79:
+                            self.rle_remaining += b - 47
+                        elif 80 <= b <= 127:
+                            self.rle_remaining += (b - 78) * 32
+                        else:
+                            raise RuntimeError(f"Unexpected byte value {b:02x}")
+                        continue
+
+
+            if self.last_sample is None:
+                self.last_sample = sample
+            self.samplenum += 1
+            self.matched = [True] * len(conds)
+            for i, cond in enumerate(conds):
+                if cond_matches(cond, self.last_sample, sample):
+                    self.matched[i] = True
+            self.last_sample = sample
+
+        bits = []
+        for b in range(len(self.logic_channels)):
+            bits.append((sample >> b) & 0x1)
+
+        return tuple(bits)
